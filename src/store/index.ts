21051eccb779b6d142b0fd4a7ea8e62960a2c272
Ondřej Tesař <ondrej.tesar@domat.cz>
2022-02-21 23:30:09 +0100

    Add Pinia

---
+++ src/store/index.ts
@@ -0,0 +1,49 @@
+import { defineStore } from "pinia";
+
+export type RootState = {
+  todos: Todo[];
+};
+
+export const useMainStore = defineStore({
+  id: "mainStore",
+  state: () =>
+    ({
+      todos: [],
+    } as RootState),
+
+  actions: {
+    createNewTodo(todo: Todo) {
+      if (!todo) {
+        return;
+      }
+
+      this.todos.push(todo);
+    },
+
+    updateTodo(id: string, payload: Todo) {
+      if (!id || !payload) {
+        return;
+      }
+
+      const index = this.findIndexById(id);
+
+      if (index !== -1) {
+        this.todos[index] = payload;
+      }
+    },
+
+    deleteTodo(id: string) {
+      const index = this.findIndexById(id);
+
+      if (index === -1) {
+        return;
+      }
+
+      this.todos.splice(index, 1);
+    },
+
+    findIndexById(id: string) {
+      return this.todos.findIndex((todo) => todo.id === id);
+    },
+  },
+});
